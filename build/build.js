6c0707287b6aa61fe548cf18a949d7e7b4a8295c
jakubkurdziel <jakub.kurdziel@talkie.ai>
2023-04-24 19:02:40 +0200

    handle displaying stats when quantity of players is greater than 10

--- build/build.js
+++ build/build.js
@@ -524,28 +524,43 @@ function keyReleased() {
     }
 }
 function showStats(players) {
-    players.forEach(function (player, i) {
-        push();
-        var x = 40 * i + 20;
-        fill(255);
-        textSize(8);
-        textAlign(CENTER);
-        text(limitNameChars(player.name, 8), x, CANVAS_HEIGHT - 40);
-        textSize(10);
-        fill(0, 255, 0);
-        text(player.getStats().kills, x - 10, CANVAS_HEIGHT - 10);
-        fill(255, 40, 40);
-        text(player.getStats().deaths, x + 10, CANVAS_HEIGHT - 10);
-        rectMode(CENTER);
-        fill(player.color);
-        rect(x, CANVAS_HEIGHT - 30, 16, 12);
-        fill(0);
-        rect(x - 6, CANVAS_HEIGHT - 30, 8, 5);
-        pop();
-    });
+    if (players.length <= 10) {
+        players.forEach(function (player, i) {
+            push();
+            var x = 40 * i + 20;
+            fill(255);
+            textSize(8);
+            textAlign(CENTER);
+            text(limitNameChars(player.name, 8), x, CANVAS_HEIGHT - 40);
+            textSize(10);
+            fill(0, 255, 0);
+            text(player.getStats().kills, x - 10, CANVAS_HEIGHT - 10);
+            fill(255, 40, 40);
+            text(player.getStats().deaths, x + 10, CANVAS_HEIGHT - 10);
+            rectMode(CENTER);
+            fill(player.color);
+            rect(x, CANVAS_HEIGHT - 30, 16, 12);
+            fill(0);
+            rect(x - 6, CANVAS_HEIGHT - 30, 8, 5);
+            pop();
+        });
+    }
+    else {
+        players.forEach(function (player, i) {
+            push();
+            var x = i >= 10 ? 40 * (i - 10) + 20 : 40 * i + 20;
+            var y = i >= 10 ? CANVAS_HEIGHT - 20 : CANVAS_HEIGHT - 40;
+            fill(255);
+            textSize(8);
+            textAlign(CENTER);
+            var textStat = limitNameChars(player.name, 5) + ": " + player.getStats().kills + "/" + player.getStats().deaths;
+            text(textStat, x, y);
+            pop();
+        });
+    }
 }
 function limitNameChars(str, end) {
-    return str.length > end ? str.slice(0, end) + '..' : str;
+    return str.length > end ? str.slice(0, end) + '.' : str;
 }
 function generateWallObjects(walls) {
     return walls.map(function (wall) { return new Wall(wall.x, wall.y, wall.width, wall.height, 'gray'); });
--- sketch/game/stats.ts
+++ sketch/game/stats.ts
@@ -1,29 +1,51 @@
 function showStats(players: Tank[]) {
-    players.forEach((player, i) => {
-        push();
-        const x = 40 * i + 20;
-        fill(255)
-        textSize(8);
-        textAlign(CENTER);
-        text(limitNameChars(player.name, 8), x, CANVAS_HEIGHT - 40);
+    if (players.length <= 10) {
+        players.forEach((player, i) => {
+            push();
+            const x = 40 * i + 20;
 
-        textSize(10);
-        fill(0, 255, 0);
-        text(player.getStats().kills, x - 10, CANVAS_HEIGHT - 10);
+            fill(255)
+            textSize(8);
+            textAlign(CENTER);
+            text(limitNameChars(player.name, 8), x, CANVAS_HEIGHT - 40);
 
-        fill(255, 40, 40);
-        text(player.getStats().deaths, x + 10, CANVAS_HEIGHT - 10);
+            textSize(10);
+            fill(0, 255, 0);
+            text(player.getStats().kills, x - 10, CANVAS_HEIGHT - 10);
 
-        rectMode(CENTER);
-        fill(player.color);
-        rect(x, CANVAS_HEIGHT - 30, 16, 12);
-        fill(0);
-        rect(x - 6, CANVAS_HEIGHT - 30, 8, 5);
+            fill(255, 40, 40);
+            text(player.getStats().deaths, x + 10, CANVAS_HEIGHT - 10);
 
-        pop();
-    });
+
+            rectMode(CENTER);
+            fill(player.color);
+            rect(x, CANVAS_HEIGHT - 30, 16, 12);
+            fill(0);
+            rect(x - 6, CANVAS_HEIGHT - 30, 8, 5);
+
+
+            pop();
+        });
+    }
+    else {
+        players.forEach((player, i) => {
+            push();
+            const x = i >= 10 ? 40 * (i - 10) + 20 : 40 * i + 20;
+            const y = i >= 10 ? CANVAS_HEIGHT - 20 : CANVAS_HEIGHT - 40;
+
+            fill(255)
+            textSize(8);
+            textAlign(CENTER);
+            const textStat = `${limitNameChars(player.name, 5)}: ${player.getStats().kills}/${player.getStats().deaths}`;
+            text(textStat, x, y);
+            pop();
+        });
+
+    }
+
 }
 
+
 function limitNameChars(str: string, end: number) {
-    return str.length > end ? str.slice(0, end) + '..' : str;
+    return str.length > end ? str.slice(0, end) + '.' : str;
 }
